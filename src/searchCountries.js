d7c00a9267633ffcb0a12cb31e011416c45b0e9a
AlenaKiporuk <alenakiporyk@gmail.com>
2022-12-11 06:06:41 +0200

    update

--- src/searchCountries.js
+++ src/searchCountries.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import { fetchCountries } from '../fetchCountries';
+import { fetchCountries } from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 
@@ -14,65 +14,71 @@ const refs = {
 refs.inputEl.addEventListener('input', debounce(onSerchInput, DEBOUNCE_DELAY));
 
 function onSerchInput() {
- 
-    refs.cardEl.innerHTML = '';
-    refs.listEl.innerHTML = '';
-  
-  fetchCountries(refs.inputEl.value)
-    .then(countries => {
-      if (countries.length > 10) {
-        Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
+  const trimmedValue = refs.inputEl.value.trim();
+  refs.cardEl.innerHTML = '';
+  refs.listEl.innerHTML = '';
+  if (trimmedValue !== '') {
+    fetchCountries(trimmedValue)
+      .then(countries => {
+        if (countries.length > 10) {
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
        
-        return;
-      }
+          return;
+        }
+        if (countries.length <= 10) {
+          renderCountriesList(countries);
+          refs.cardEl.innerHTML = '';
+        }
 
-      if (countries.length <= 10) {
-        refs.listEl.innerHTML = countries
-          .map(country => {
-            return `
+        if (countries.length === 1) {
+          cardCountry(countries);
+          refs.listEl.innerHTML = '';
+        }
+        if (countries.length === 0) {
+          Notiflix.Notify.failure('Oops, there is no country with that name');
+        }
+      })
+      .catch(error => {
+        Notify.failure('Oops, there is no country with that name');
+     
+        return error;
+      });
+  }
+};
+function renderCountriesList(countries)
+{refs.listEl.innerHTML = countries
+            .map(country => {
+              return `
   <li class="country-list__item">
     <img class="country-list__flags" src="${country.flags.svg}" alt="${country.name.official}" width="50" />
     <h2 class="country-list__name">${country.name.official}</h2>
   </li>
   `;
-          })
-          .join('');
-        refs.cardEl.innerHTML = '';
-      }
-
-      if (countries.length === 1) {
-        refs.cardEl.innerHTML = countries
-          .map(country => {
-            console.log(country);
-            return `
+            })
+            .join('');
+};
+function cardCountry(countries) {
+   refs.cardEl.innerHTML = countries
+            .map(country => {
+              console.log(country);
+              return `
     <div class="country-info__container">
       <div class="country-info__wrapper">
-        <img class="country-info__flags" src="${country.flags.svg}" alt="${
-              country.name.official
-            }" width="100" />
+        <img class="country-info__flags" src="${country.flags.svg}" alt="${country.name.official
+                }" width="100" />
         <h2 class="country-info__name">${country.name.official}</h2>
       </div>
-      <p class="country-info__capital"><span class="country-info__weight">Capital:</span> ${
-        country.capital
-      }</p>
-      <p class="country-info__population"><span class="country-info__weight">Population:</span> ${
-        country.population
-      }</p>
+      <p class="country-info__capital"><span class="country-info__weight">Capital:</span> ${country.capital
+                }</p>
+      <p class="country-info__population"><span class="country-info__weight">Population:</span> ${country.population
+                }</p>
       <p class="country-info__languages"><span class="country-info__weight">Languages:</span> ${Object.values(
-        country.languages
-      )}</p>
+                  country.languages
+                )}</p>
     </div>
   `;
-          })
-          .join('');
-        refs.listEl.innerHTML = '';
-      }
-    })
-    .catch(error => {
-      Notify.failure('Oops, there is no country with that name');
-     
-      return error;
-    });
+            })
+            .join('');
 }
